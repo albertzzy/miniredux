8bc45a42f9958892f3ca4d725f302b1b4e113771
albertzzy <zhaozi_ye_hi@126.com>
2017-04-20 14:11:01 +0800

    update

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-## a simple state manager highly inspired by redux
+## a simple state manager highly inspired by redux(which is still being constructed)
+
+	### regardless of tracking-difference / immutable recommanded
 
 state
 	dispatch(actionPayload,callback[prev,cur]) 
-		reducer => curState
+		reducer
 	subscribe // global watch
 
 combineReducer
+	reducer managament
+
--- combineReducer.js
+++ combineReducer.js
@@ -1,6 +1,47 @@
-export default function combineReducers(...reducers){
-	
+const isWindow(obj){
+	return obj!== null && obj === obj.window;
+}
+
+const isObject(obj){
+	return typeof obj === 'object';
+}
+
+const isplainObject(obj){
+	return !isWindow(obj) && isObject(obj) && Object.getPrototypeOf(obj) === Object.prototype;
+}
+
+export default function combineReducers(reducers){
+	if(isplainObject(reducers)){
+		throw new Error('param should be a plain object');
+	}
 
 
+	var reducersKeys = Object.keys(reducers);
+
+	return function(state={},action){
+
+		for(var i=0;i<reducersKeys.length;i++){
+
+			var reducerKey = reducersKeys[i];
+			var reducer = reducers[reducerKey];
+
+			if(typeof reducer !== 'function'){
+
+				throw new Error('param should be a function.');
+			}
+
+
+			var previousStateForKey = state[reducerKey];
+			var currentStateForKey = reducer(previousStateForKey,action);
+
+			if(previousStateForKey!== currentStateForKey){
+				state[reducerKey] = currentStateForKey;
+			}
+
+		}
+
+		return state;
+
+	}
 
 }
